a08c8b65f7104f5a5b1385963255a2e86024eef8
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-11-11 23:23:27 +0600

    ✅ configure fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				nunito: ["sans-serif", "Nunito Sans"],
+			},
 			colors: {
 				primary: "#209E2E",
 				secondary: "#252525",
 				mutated: "#808080",
+				light: "#F4F4F4",
 			},
 		},
 	},
